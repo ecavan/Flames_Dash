ac860dfffa568884954934c8e8fa4c369b2352c9
Elijah Cavan <eli_cavan@live.ca>
2021-11-15 17:32:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -271,4 +271,4 @@ def update_output(value,value2, value3):
 
 if __name__ == '__main__':
     #app.run_server(host='0.0.0.0', port=8050, debug=False)
-    app.run_server(debug=True)
+    app.run_server()
