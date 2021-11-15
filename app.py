be03047345264f22cafca51e8ec7bb98774b8596
Elijah Cavan <eli_cavan@live.ca>
2021-11-15 17:27:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from hockey_rink import NHLRink, IIHFRink, NWHLRink
+#from hockey_rink import NHLRink, IIHFRink, NWHLRink
 from dash.dependencies import Input, Output
 import plotly.express as px
 import matplotlib.pyplot as plt
