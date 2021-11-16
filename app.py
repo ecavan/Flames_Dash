9f1319a4c9d8109e8cd5e3e90dd1bb918abdbc50
Elijah Cavan <eli_cavan@live.ca>
2021-11-16 02:47:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,15 +28,12 @@ df.loc[(df['Period'] == 1)&(df.Team == df['Away Team']), 'X Coordinate 2'] = 200
 df.loc[(df['Period'] == 3)&(df.Team == df['Away Team']), 'X Coordinate 2'] = 200 - df['X Coordinate 2']
 df.loc[(df['Period'] == 2)&(df.Team == df['Home Team']), 'X Coordinate 2'] = 200 - df['X Coordinate 2']
 
-
 available_teams = df['Team'].unique()
 available_dates = df['game_date'].unique()
 
-
-#fig = px.density_heatmap(df, x="X Coordinate", y="Y Coordinate", color = "Team")
-
 app = dash.Dash()
 server = app.server
+
 app.layout = html.Div(children=[
     
     html.H1(children='Calgary Flames Hockey Analytics Dashboard!'),
@@ -48,6 +45,13 @@ app.layout = html.Div(children=[
         id='demo-dropdown',
         options=[{'label': k, 'value': k} for k in available_teams],
         value=['Olympic (Women) - Canada'],
+        multi=True
+    ),
+        
+    dcc.Dropdown(
+        id='demo-dropdownd',
+        options=[{'label': k, 'value': k} for k in available_dates],
+        value=['2018-02-11'],
         multi=True
     ),
 
@@ -71,12 +75,19 @@ app.layout = html.Div(children=[
 
 ]),
     
-    html.Div(children='''Analytics to help Creators grow their engagements'''),
+    html.Div(children='''This plots shows the X,Y Coordinates where Passes Happen most often'''),
     html.Div([
     dcc.Dropdown(
         id='demo-dropdown2',
         options=[{'label': k, 'value': k} for k in available_teams],
         value=['Olympic (Women) - Canada'],
+        multi=True
+    ),
+        
+    dcc.Dropdown(
+        id='demo-dropdownd2',
+        options=[{'label': k, 'value': k} for k in available_dates],
+        value=['2018-02-11'],
         multi=True
     ),
         
@@ -97,6 +108,7 @@ app.layout = html.Div(children=[
     html.Hr(),
     dcc.Graph(id='display-selected-values2')]),
     
+    html.Div(children='''This plots shows which shot types are optimal for each team'''),
     html.Div([
     dcc.Dropdown(
         id='demo-dropdown3',
@@ -108,27 +120,7 @@ app.layout = html.Div(children=[
     html.Hr(),
     dcc.Graph(id='display-selected-values3')]),
     
-    
-    html.Div([
-    dcc.Dropdown(
-        id='demo-dropdown4',
-        options=[{'label': k, 'value': k} for k in available_teams],
-        value=['Olympic (Women) - Canada'],
-        multi=True
-    ),
-        
-    html.Div([
-    dcc.Dropdown(
-        id='demo-dropdown5',
-        options=[{'label': k, 'value': k} for k in available_dates],
-        value=['2018-02-11'],
-        multi=True
-    ),
-
-    html.Hr(),
-    dcc.Graph(id='display-selected-values4')])
-    
-]),
+    html.Div(children='''This plots shows the X,Y Coordinates where Takeaways/Puck Recoveries Happen'''),
     html.Div([
     dcc.Dropdown(
         id='demo-dropdown6',
@@ -168,75 +160,61 @@ app.layout = html.Div(children=[
 @app.callback(
     dash.dependencies.Output('display-selected-values', 'figure'),
     [dash.dependencies.Input('demo-dropdown', 'value'),
+     dash.dependencies.Input('demo-dropdownd', 'value'),
     dash.dependencies.Input('year-slider', 'value')])
 
-def update_output(value,value2):
-    ts = df[(df["Team"].isin(value))&(df.Event == 'Shot')&(df.Period == value2)]
+def update_output(value,value2,value3):
+    ts = df[(df["Team"].isin(value))&(df.Event == 'Shot')&(df.Period == value3)]
+    ts = ts[ts.game_date.isin(value2)]
+    
+    ts2 = df[(df["Team"].isin(value))&(df.Event == 'Goal')&(df.Period == value3)]
+    ts2 = ts2[ts2.game_date.isin(value2)]
     fig = px.scatter(ts, x="X Coordinate", y="Y Coordinate", color = "Team", title = 'Shot Plot For Each Period')
+    fig.add_scatter(x=ts2["X Coordinate"], y=ts2["Y Coordinate"], mode='markers', name="Goals")
+    
     return fig
 
 
 @app.callback(
     dash.dependencies.Output('display-selected-values2', 'figure'),
     [dash.dependencies.Input('demo-dropdown2', 'value'),
-    dash.dependencies.Input('year-slider2', 'value')])
+    dash.dependencies.Input('year-slider2', 'value'),
+    dash.dependencies.Input('demo-dropdownd2', 'value')])
 
 
-def update_output(value,value2 ):
+def update_output(value,value2,value3):
     ts = df[(df["Team"].isin(value))&(df.Event == 'Play')&(df.Period == value2)]
+    ts = ts[ts.game_date.isin(value3)]
     ts = ts.dropna(subset = ["X Coordinate 2"])
-    fig = px.density_heatmap(ts, x="X Coordinate 2", y="Y Coordinate 2", title = 'Pass Plot Heatmap')
+
+    fig1 = px.line(ts, x="X Coordinate", y="Y Coordinate")
+    fig4 = px.line(ts, x="X Coordinate 2", y="Y Coordinate 2")
+    fig2 = px.scatter(ts, x="X Coordinate", y="Y Coordinate", color_discrete_sequence=['red'])
+    fig3 = px.scatter(ts, x="X Coordinate 2", y="Y Coordinate 2", color_discrete_sequence=['red'])
+    
+    fig = go.Figure(data=fig1.data + fig2.data + fig3.data + fig4.data)
+    fig.update_layout(title = 'Pass Plot by Period', xaxis_title="X Coordinate",
+    yaxis_title="Y Coordinate")
+
     return fig
 
 @app.callback(
     dash.dependencies.Output('display-selected-values3', 'figure'),
     [dash.dependencies.Input('demo-dropdown3', 'value')])
 
-def update_output(value):
-#     df.loc[(df.Event == 'Shot')&(df['Detail 2'] == 'On Net')&(df.Team == df['Home Team']), 'home_shot'] = 1
-#     df.loc[(df.Event == 'Shot')&(df['Detail 2'] == 'On Net')&(df.Team == df['Away Team']), 'away_shot'] = 1
-
-#     df.loc[(df.Team == df['Home Team']), 'shots'] = df['home_shot']
-#     df.loc[(df.Team == df['Away Team']), 'shots'] = df['away_shot']
+def update_output(value):    
+    df.loc[(df['Home Team'].isin(value)), 'goals'] = df['Home Team Goals']
+    df.loc[( df['Home Team'].isin(value)), 'goals'] = df['Away Team Goals']
     
-    df.loc[(df['Home Team'] == 'Olympic (Women) - Canada'), 'goals'] = df['Home Team Goals']
-    df.loc[( df['Home Team'] == 'Olympic (Women) - Canada'), 'goals'] = df['Away Team Goals']
-    
-    df_c = df[df.Team == 'Olympic (Women) - Canada']
-
-#    df_c['SOG'] = df_c.groupby(['Team'])['shots'].transform('sum')
-
+    df_c = df[df.Team.isin(value)]
 
     df_shot = df_c[df_c.Event == 'Shot']
-
-
-#    gb = (df_shot.groupby(['Team', 'Detail 1'])['goals'].sum())/df_shot.groupby(['Team', 'Detail 1'])['SOG'].sum()
-#    gb = gb.reset_index(name = 'Shooting %')
-    #gb_team = gb[gb.Team == value]
     
-#    df_shot = df_shot.merge(gb, on = ['Team', 'Detail 1'])
-    
-    df_shot = df_shot[df_shot.Team == 'Olympic (Women) - Canada']
+    df_shot = df_shot[df_shot.Team.isin(value)]
     
     fig = px.sunburst(df_shot, path=['Detail 1', 'Detail 2'], values='goals', title="Shooting Profile by Shot Type")
     return fig
     
-    
-@app.callback(
-    dash.dependencies.Output('display-selected-values4', 'figure'),
-    [dash.dependencies.Input('demo-dropdown4', 'value'),
-    dash.dependencies.Input('demo-dropdown5', 'value')])
-
-
-def update_output(value,value2):
-    df_cm = df[(df.Event == 'Shot')&(df['Detail 2'] == 'On Net')&(df.Team == 'Olympic (Women) - Canada')&(df.game_date == '2018-02-11')]
-    df_cm.loc[(df_cm.Event == 'Shot')&(df_cm['Detail 2'] == 'On Net'), 'shot'] = 1
-    df_cm = df_cm.sort_values('time')
-
-    df_cm['Shots on Goal'] = df_cm['shot'].cumsum()
-    fig = px.line(df_cm, x='time', y="Shots on Goal", title="Culmative Shots over Time")
-    
-    return fig
 
 
 @app.callback(
@@ -247,29 +225,17 @@ def update_output(value,value2):
 
 
 def update_output(value,value2, value3):
-    ts = df[(df.Event == 'Puck Recovery')&(df.Team == 'Olympic (Women) - Canada')&(df.Period == value3)&(df.game_date == '2018-02-11')]
-    ts2 = df[(df.Event == 'Takeaway')&(df.Team == 'Olympic (Women) - Canada')&(df.Period == value3)&(df.game_date == '2018-02-11')]
     
-    fig = go.Figure()
+    ts = df[(df.Event == 'Puck Recovery')&(df.Team.isin(value))&(df.Period == value3)]
+    ts = ts[ts.game_date.isin(value2)]
+    
+    ts2 = df[(df.Event == 'Takeaway')&(df.Team.isin(value))&(df.Period == value3)]
+    ts2 = ts2[ts2.game_date.isin(value2)]
 
-    fig.add_trace(
-        go.Scatter(
-            x=ts["X Coordinate"],
-            y=ts["Y Coordinate"],
-            mode='markers',
-            name = 'Puck Recovery'
-        ))
-
-    fig.add_trace(
-        go.Scatter(
-            x=ts2["X Coordinate"],
-            y=ts2["Y Coordinate"],
-            mode='markers',
-            name = 'Takeaways'
-        ))
-    fig.update_layout(title = 'Recoveries/ Takeaways by Team')
+    fig = px.scatter(ts, x="X Coordinate", y="Y Coordinate", color = "Team", title = 'Recoveries/ Takeaways by Team')
+    fig.add_scatter(x=ts2["X Coordinate"], y=ts2["Y Coordinate"], mode='markers', name="Takeaway",  text=ts2["Team"])
+    
     return fig
 
 if __name__ == '__main__':
-    #app.run_server(host='0.0.0.0', port=8050, debug=False)
     app.run_server()
